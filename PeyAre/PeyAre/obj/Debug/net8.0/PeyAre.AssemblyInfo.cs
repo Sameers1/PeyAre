3cb4614b1092fb97d52b025119cd21fe7fc9dceb
Sameers1 <51760171+Sameers1@users.noreply.github.com>
2024-11-05 13:15:31 +0500

    all of the training type has its own foarm / view like conditonal rendering

--- PeyAre/PeyAre/obj/Debug/net8.0/PeyAre.AssemblyInfo.cs
+++ PeyAre/PeyAre/obj/Debug/net8.0/PeyAre.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeyAre")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ee00c71ff9529e603330935438068c779bfd334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3ee6a7e57966690fd9929bc026b8c34dbec026d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeyAre")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeyAre")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
