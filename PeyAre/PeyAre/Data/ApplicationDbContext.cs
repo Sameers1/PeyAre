fe34585519ae32155f4667377a8ea90568906478
Sameers1 <51760171+Sameers1@users.noreply.github.com>
2024-11-06 05:38:14 +0500

    finally after 4 hour... app is connected to database.

---
+++ PeyAre/PeyAre/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeyAre.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    // Define DbSets for your entities
+    public DbSet<HistoryRPM> HistoryRPMs { get; set; }
+}
---
+++ PeyAre/PeyAre/Migrations/20241106002753_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeyAre.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HistoryRPMs",
+                columns: table => new
+                {
+                    HistoryRPMId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    lift = table.Column<int>(type: "int", nullable: false),
+                    reps = table.Column<int>(type: "int", nullable: false),
+                    SelectedLift = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AvailableLifts = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistoryRPMs", x => x.HistoryRPMId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HistoryRPMs");
+        }
+    }
+}
