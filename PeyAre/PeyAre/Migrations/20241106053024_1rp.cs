b7f36cdb8d83ae031fe68088212f6d696fbade5f
Sameers1 <51760171+Sameers1@users.noreply.github.com>
2024-11-06 12:07:57 +0500

    1

---
+++ PeyAre/PeyAre/Migrations/20241106053024_1rp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeyAre.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1rp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RepMaxes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LiftName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WeightLifted = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Reps = table.Column<int>(type: "int", nullable: false),
+                    OneRepMax = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RepMaxes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RepMaxes");
+        }
+    }
+}
---
+++ PeyAre/PeyAre/Models/RepMax.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeyAre.Data
+{
+    public class RepMax
+    {
+        public int Id { get; set; }
+        public string LiftName { get; set; }   // The name of the lift (e.g., "Bench Press")
+        public decimal WeightLifted { get; set; }  // The weight lifted
+        public int Reps { get; set; }         // The number of reps performed
+        public decimal OneRepMax { get; set; }   // The calculated 1RM
+        public DateTime Date { get; set; }        // Date of the lift attempt
+    }
+
+}
