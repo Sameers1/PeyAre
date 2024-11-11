c5c482b8ee8d167f6ef43aa7841d13ea55d87f38
Sameers1 <51760171+Sameers1@users.noreply.github.com>
2024-11-11 12:01:11 +0500

    applied mudblazor on apidata1 and its saving data to database and showing in decending order

--- PeyAre/PeyAre/obj/Debug/net8.0/PeyAre.AssemblyInfo.cs
+++ PeyAre/PeyAre/obj/Debug/net8.0/PeyAre.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeyAre")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebd8fa4e7e3a251eedff1e5b6f949badb21759a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ecae9c69eac259ab674654e5c698445c28a53c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeyAre")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeyAre")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
